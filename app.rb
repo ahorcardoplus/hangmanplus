a64d433a5101bb339870c35be432396cba644709
kleer <kleer@kleer-csd-box>
2017-02-03 15:01:07 -0500

    result page

--- app.rb
+++ app.rb
@@ -26,6 +26,11 @@ post "/guess" do
   letter = params[:letter]
   hangman = session["hangman"]
   @message = hangman.guess(letter)
+
+  if hangman.game_status == :won or hangman.game_status == :lost
+    return erb :result
+  end
+
   @missed_letters = hangman.missed_letters()
   @remainig_attempts = hangman.remaining_fail_attempts()
 
